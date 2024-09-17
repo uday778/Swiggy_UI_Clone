6346ab00272ae2b23e80ebd4486aeab2f3e478e3
uday778 <uday33378@gmail.com>
2024-09-17 14:38:28 +0530

    Added main files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Head />}>
+        <Route path="/" element={<Body />}/>
+        <Route path="/restaurantmenu/:id" element={<RestaurantMenu />}/>
+      </Route>
+    </Routes>
+
+    // <>
+    //   <div>
+    //    <Head/>
+    //    <Body/>
+
+    //   </div>
+    // </>
+  );
+}
+
+export default App;
